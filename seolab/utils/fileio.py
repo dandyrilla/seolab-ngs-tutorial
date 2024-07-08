3b530f221aedf9f4b5ae107b512da9410b636503
Sukjun Kim <dandyrilla@naver.com>
2024-07-08 21:17:34 +0900

    Add two utility functions related to TSV format

--- seolab/utils/fileio.py
+++ seolab/utils/fileio.py
@@ -1,6 +1,22 @@
 import os
 
+import pandas as pd
+
 
 def make_parent_dir(path):
     parent_dir = os.path.dirname(os.path.abspath(os.path.normpath(path)))
     os.makedirs(parent_dir, exist_ok=True)
+
+
+def read_tsv(filename, **kw):
+    _kw = dict(sep='\t', header=0)
+    _kw.update(kw)
+    df = pd.read_csv(filename, **_kw)
+    return df
+
+
+def to_tsv(df, filename, **kw):
+    make_parent_dir(filename)
+    _kw = dict(sep='\t', index=False)
+    _kw.update(kw)
+    df.to_csv(filename, **_kw)
